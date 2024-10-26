db713a811c3736df27da707d64a8798f968113fe
ferrisbueller66 <tmtarpinian@gmail.com>
2024-10-25 22:54:07 -0400

    update navbar for v1 deploy

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,30 +1,31 @@
 import { Link } from "react-router-dom";
 import './navbar.css';
-import logo from './assets/logo-r.png';
+import logo from './assets/white-rectangle-logo.webp'
 
 const NavBar =  ()=> {
   return(
     <header>
       <nav className="navbar">
+        <div class="container-fluid px-3">
+          <Link to="/" className="navbar-brand">
+              <img src={logo} alt="CleanfFix Services Logo which includes a an orange fox in blue coverals holding a wrench standing next to the title CleanFix Services" className="img-fluid"/>
+          </Link>
+            
           <ul className="navbar-nav list-group list-group-horizontal ml-2">
-            <li className="nav-item active">
-              <Link to="/" className="navbar-brand">
-                  <img src={logo} alt="CleanfFix Services Fox Logo" style={{width: '10rem'}}/>
-              </Link>
-            </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link to="/about" className="nav-link">About</Link>
-            </li>
+            </li> */}
             <li className="nav-item">
               <Link to="/about" className="nav-link">Services</Link>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link to="/calculators" className="nav-link">Testimonials</Link>
-            </li>
+            </li> */}
             <li className="nav-item">
               <Link to="/calculators" className="nav-link">Calculators</Link>
             </li>
           </ul>
+          </div>
       </nav>
     </header>
   )
